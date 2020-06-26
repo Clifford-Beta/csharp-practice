3854a12e8b1b0d5382db55acc78c779a2b2a0fbe
Clifford Beta <betaclifford@gmail.com>
2020-06-26 08:19:05 +0300

    ##### Learning C# ######  String operations - interpolation, searching ...

---
+++ practice/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string greeting = "     Hello World!         ";
+            Console.WriteLine($"[{greeting}]");
+            string trimmedGreeting = greeting.TrimStart();
+            Console.WriteLine($"[{trimmedGreeting}]");
+            trimmedGreeting = greeting.TrimEnd();
+            Console.WriteLine($"[{trimmedGreeting}]");
+            trimmedGreeting = greeting.Trim();
+            Console.WriteLine($"[{trimmedGreeting}]");
+
+            string sayHello = "Hello World!";
+            Console.WriteLine(sayHello);
+            sayHello = sayHello.Replace("Hello", "Greetings");
+            Console.WriteLine(sayHello);
+            Console.WriteLine(sayHello.ToLower());
+            Console.WriteLine(sayHello.ToUpper());
+
+            string songLyrics = "You say goodbye, and I say hello.";
+            string goodBye = "goodbye";
+            string greetings = "greetings";
+            if (songLyrics.Contains(goodBye))
+            {
+                Console.WriteLine($"Song lyrics contains {goodBye}");
+            }
+
+            if (songLyrics.Contains(greetings))
+            {
+                Console.WriteLine($"Song lyrics contains {greetings}");
+            }
+
+            if (songLyrics.StartsWith("You"))
+            {
+                Console.WriteLine("You are the subject of this song");
+            }
+
+            if (songLyrics.EndsWith("."))
+            {
+                Console.WriteLine("This song ends with a period");
+            }
+
+        }
+    }
+}
